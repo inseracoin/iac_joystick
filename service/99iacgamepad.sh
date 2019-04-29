9181e6375eb73f71cd24b4ba7094c83e4eab635b
Alexandre Ribeito de Sá <alexandre@vortexstudios.com>
2019-04-29 18:51:51 -0300

    inid.d service

---
+++ service/99iacgamepad.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+### BEGIN INIT INFO
+# Provides:          insert a coin
+# Required-Start:    
+# Required-Stop:     
+# Default-Start:     
+# Default-Stop:      
+# Short-Description: iac gamepad
+# Description:       Run Insert a Coin gamepad service
+### END INIT INFO
+
+# Carry out specific functions when asked to by the system
+/bin/iacgamepad
+
+exit 0
